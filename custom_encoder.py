b3fe0f13bcd752edaf616f8227766dee3f22353c
Alex Ashworth <aashworth@bignerdranch.com>
2023-12-07 10:47:20 -0500

    cleaning up function and encoder syntax errors

--- custom_encoder.py
+++ custom_encoder.py
@@ -1,6 +1,8 @@
 import json
 from decimal import Decimal
-class CustomEncoder(json.JSONENCODER):
+
+
+class CustomEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, Decimal):
             return float(obj)
